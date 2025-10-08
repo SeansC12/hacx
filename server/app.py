d74e5267b89b87f2cef7109a302239369b28d3a0
Sean Chua <88256324+SeansC12@users.noreply.github.com>
2025-10-08 22:55:24 +0800

    Project setup

---
+++ server/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/api/healthcheck/", methods=["GET"])
+def healthcheck():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
