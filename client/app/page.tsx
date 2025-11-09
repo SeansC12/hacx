0148864ac07bdab848ea505d97138e4aaf8dbb4e
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2025-11-09 22:28:59 +0800

    Changed the banner to look nicer

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,20 +6,11 @@ import LostItemImage from "@/public/homepage/lost-item.jpg";
 import TrafficMatterImage from "@/public/homepage/traffic-matters.png";
 import LodgeReportImage from "@/public/homepage/lodge-report.webp";
 import Banner from "@/components/banner";
-import { MessageSquareWarningIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center flex-col w-full h-dvh">
       <Banner />
-      <div className="flex flex-col items-center pt-10 gap-4">
-        <p className="text-4xl font-semibold">
-          Welcome to the Singapore Police Force Kiosk!
-        </p>
-        <h2>
-          Please select an option below to continue:
-        </h2>
-      </div>
       <div className="flex-1 w-full flex items-center justify-between gap-4 px-16">
         <ButtonCard
           title="Traffic Matters"
--- client/components/banner.tsx
+++ client/components/banner.tsx
@@ -4,9 +4,14 @@ import bannerSvg from "@/public/homepage/brand-isolated.svg";
 export default function Banner() {
   return (
     <div className="w-full h-48 bg-gradient-to-r from-blue-primary to-blue-secondary flex items-center justify-between px-72 shadow-md">
-      <h1 className="text-7xl text-white font-extrabold">
-        Woodleigh Neighbourhood Police Post
-      </h1>
+      <div className="text-white flex flex-col gap-4">
+        <h1 className="text-7xl  font-extrabold">
+          Woodleigh Neighbourhood Police Post
+        </h1>
+        <h2 className="text-2xl">
+          Please select an option below to continue.
+        </h2>
+      </div>
       <Image src={bannerSvg} alt="Woodleigh NPP Logo" className="h-48" />
     </div>
   );
